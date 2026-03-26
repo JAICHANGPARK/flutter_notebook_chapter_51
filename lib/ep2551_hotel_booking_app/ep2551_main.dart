0a7b1960c533feee7cb88d07dba06d227a38b4b8
JAICHANGPARK <aristojeff@gmail.com>
2026-03-26 18:46:48 +0900

    Update ep2551_main.dart

--- lib/ep2551_hotel_booking_app/ep2551_main.dart
+++ lib/ep2551_hotel_booking_app/ep2551_main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_51/ep2551_hotel_booking_app/hotel_booking_app.dart';
 
 void main() {
-  
+  runApp(HotelBookingApp());
 }
