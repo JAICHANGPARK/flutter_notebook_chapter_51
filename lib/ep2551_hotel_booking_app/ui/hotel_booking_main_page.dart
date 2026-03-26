b940e48ece9ce5e5b07540e57e2f04c1396d9f05
JAICHANGPARK <aristojeff@gmail.com>
2026-03-26 18:41:44 +0900

    Update hotel_booking_main_page.dart

--- lib/ep2551_hotel_booking_app/ui/hotel_booking_main_page.dart
+++ lib/ep2551_hotel_booking_app/ui/hotel_booking_main_page.dart
@@ -11,6 +11,12 @@ class HotelBookingMainPage extends StatefulWidget {
 class _HotelBookingMainPageState extends State<HotelBookingMainPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
   }
 }
