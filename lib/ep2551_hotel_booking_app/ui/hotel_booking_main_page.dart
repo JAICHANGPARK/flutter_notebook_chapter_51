04ab500a47648ca1fd6b9a4ee0dd6ae630aa47a4
JAICHANGPARK <aristojeff@gmail.com>
2026-03-29 10:09:45 +0900

    Update hotel_booking_main_page.dart

--- lib/ep2551_hotel_booking_app/ui/hotel_booking_main_page.dart
+++ lib/ep2551_hotel_booking_app/ui/hotel_booking_main_page.dart
@@ -92,165 +92,194 @@ class _HotelBookingMainPageState extends State<HotelBookingMainPage> {
                       ],
                     ),
                   ),
-                  Expanded(child: SingleChildScrollView(child: Column(children: [
-
-                    Column(
-                      spacing: 12,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 16),
-                          child: Row(
-                            mainAxisAlignment: .spaceBetween,
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          Column(
+                            spacing: 12,
                             children: [
-                              Text(
-                                "Recommended Hotel",
-                                style: TextStyle(fontWeight: .bold, fontSize: 18),
-                              ),
-                              TextButton(
-                                onPressed: () {},
-                                style: TextButton.styleFrom(
-                                  foregroundColor: Colors.blue,
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 16,
                                 ),
-                                child: Text('See all'),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Container(
-                          height: 280,
-                          child: ListView.builder(
-                            scrollDirection: .horizontal,
-                            itemBuilder: (context, index) {
-                              return Container(
-                                width: 200,
-                                margin: .only(left: 16),
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: .circular(12),
-                                ),
-                                padding: .all(12),
-                                child: Column(
-                                  crossAxisAlignment: .start,
-                                  spacing: 12,
+                                child: Row(
+                                  mainAxisAlignment: .spaceBetween,
                                   children: [
-                                    Expanded(
-                                      child: Container(
-                                        decoration: BoxDecoration(
-                                          color: Colors.blue,
-                                          borderRadius: .circular(12),
-                                        ),
+                                    Text(
+                                      "Recommended Hotel",
+                                      style: TextStyle(
+                                        fontWeight: .bold,
+                                        fontSize: 18,
                                       ),
                                     ),
-                                    Column(
-                                      spacing: 4,
-                                      crossAxisAlignment: .start,
-                                      children: [
-                                        Row(
-                                          mainAxisAlignment: .spaceBetween,
-                                          children: [
-                                            Container(
-                                              decoration: ShapeDecoration(
-                                                shape: StadiumBorder(),
-                                                color: Colors.grey[100],
-                                              ),
-                                              padding: EdgeInsets.symmetric(
-                                                horizontal: 8,
-                                                vertical: 4,
-                                              ),
-                                              child: Text(
-                                                "20%Off",
-                                                style: TextStyle(
-                                                  color: Colors.blue,
-                                                  fontSize: 10,
-                                                  fontWeight: .bold,
-                                                ),
+                                    TextButton(
+                                      onPressed: () {},
+                                      style: TextButton.styleFrom(
+                                        foregroundColor: Colors.blue,
+                                      ),
+                                      child: Text('See all'),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Container(
+                                height: 280,
+                                child: ListView.builder(
+                                  scrollDirection: .horizontal,
+                                  itemBuilder: (context, index) {
+                                    return Container(
+                                      width: 200,
+                                      margin: .only(left: 16),
+                                      decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: .circular(12),
+                                      ),
+                                      padding: .all(12),
+                                      child: Column(
+                                        crossAxisAlignment: .start,
+                                        spacing: 12,
+                                        children: [
+                                          Expanded(
+                                            child: Container(
+                                              decoration: BoxDecoration(
+                                                color: Colors.blue,
+                                                borderRadius: .circular(12),
                                               ),
                                             ),
-                                            Container(
-                                              decoration: ShapeDecoration(
-                                                shape: StadiumBorder(),
-                                                color: Colors.grey[100],
-                                              ),
-                                              padding: EdgeInsets.symmetric(
-                                                horizontal: 8,
-                                                vertical: 4,
-                                              ),
-                                              child: Row(
-                                                spacing: 4,
+                                          ),
+                                          Column(
+                                            spacing: 4,
+                                            crossAxisAlignment: .start,
+                                            children: [
+                                              Row(
+                                                mainAxisAlignment:
+                                                    .spaceBetween,
                                                 children: [
-                                                  Icon(
-                                                    Icons.star,
-                                                    size: 14,
-                                                    color: Colors.orange,
+                                                  Container(
+                                                    decoration: ShapeDecoration(
+                                                      shape: StadiumBorder(),
+                                                      color: Colors.grey[100],
+                                                    ),
+                                                    padding:
+                                                        EdgeInsets.symmetric(
+                                                          horizontal: 8,
+                                                          vertical: 4,
+                                                        ),
+                                                    child: Text(
+                                                      "20%Off",
+                                                      style: TextStyle(
+                                                        color: Colors.blue,
+                                                        fontSize: 10,
+                                                        fontWeight: .bold,
+                                                      ),
+                                                    ),
                                                   ),
-                                                  Text(
-                                                    "4.6",
-                                                    style: TextStyle(
-                                                      fontSize: 10,
+                                                  Container(
+                                                    decoration: ShapeDecoration(
+                                                      shape: StadiumBorder(),
+                                                      color: Colors.grey[100],
+                                                    ),
+                                                    padding:
+                                                        EdgeInsets.symmetric(
+                                                          horizontal: 8,
+                                                          vertical: 4,
+                                                        ),
+                                                    child: Row(
+                                                      spacing: 4,
+                                                      children: [
+                                                        Icon(
+                                                          Icons.star,
+                                                          size: 14,
+                                                          color: Colors.orange,
+                                                        ),
+                                                        Text(
+                                                          "4.6",
+                                                          style: TextStyle(
+                                                            fontSize: 10,
+                                                          ),
+                                                        ),
+                                                      ],
                                                     ),
                                                   ),
                                                 ],
                                               ),
-                                            ),
-                                          ],
-                                        ),
-                                        Text(
-                                          "OasisOverture",
-                                          style: TextStyle(fontWeight: .bold),
-                                        ),
-                                        Row(
-                                          children: [
-                                            Icon(Icons.location_on, size: 14),
-                                            Text(
-                                              "New York, USA",
-                                              style: TextStyle(fontSize: 12),
-                                            ),
-                                          ],
-                                        ),
-                                        Row(
-                                          children: [
-                                            Text("\$530"),
-                                            Text("/height"),
-                                          ],
-                                        ),
-                                      ],
-                                    ),
-                                  ],
+                                              Text(
+                                                "OasisOverture",
+                                                style: TextStyle(
+                                                  fontWeight: .bold,
+                                                ),
+                                              ),
+                                              Row(
+                                                children: [
+                                                  Icon(
+                                                    Icons.location_on,
+                                                    size: 14,
+                                                  ),
+                                                  Text(
+                                                    "New York, USA",
+                                                    style: TextStyle(
+                                                      fontSize: 12,
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
+                                              Row(
+                                                children: [
+                                                  Text("\$530"),
+                                                  Text("/height"),
+                                                ],
+                                              ),
+                                            ],
+                                          ),
+                                        ],
+                                      ),
+                                    );
+                                  },
                                 ),
-                              );
-                            },
-                          ),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 16),
-                          child: Row(
-                            mainAxisAlignment: .spaceBetween,
-                            children: [
-                              Text(
-                                "Nearby Hotel",
-                                style: TextStyle(fontWeight: .bold, fontSize: 18),
-                              ),
-                              TextButton(
-                                onPressed: () {},
-                                child: Text('See all'),
                               ),
                             ],
                           ),
-                        ),
-                        ...List.generate(5, (idx) {
-                          return Container(
-                            height: 140,
-                            child: Placeholder(),
-                          );
-                        }),
-                      ],
+                          Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 16,
+                                ),
+                                child: Row(
+                                  mainAxisAlignment: .spaceBetween,
+                                  children: [
+                                    Text(
+                                      "Nearby Hotel",
+                                      style: TextStyle(
+                                        fontWeight: .bold,
+                                        fontSize: 18,
+                                      ),
+                                    ),
+                                    TextButton(
+                                      onPressed: () {},
+                                      child: Text(
+                                        'See all',
+                                        style: TextButton.styleFrom(
+                                          foregroundColor: Colors.blue,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              ...List.generate(5, (idx) {
+                                return Container(
+                                  height: 140,
+                                  child: Placeholder(),
+                                );
+                              }),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
-                  ],),))
-
+                  ),
                 ],
               ),
             ),
