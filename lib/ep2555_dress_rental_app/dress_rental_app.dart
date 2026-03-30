ea564f8b989317401515d889910171a2855a2ae3
JAICHANGPARK <aristojeff@gmail.com>
2026-03-30 17:23:09 +0900

    Update dress_rental_app.dart

---
+++ lib/ep2555_dress_rental_app/dress_rental_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class DressRentalApp extends StatelessWidget {
+  const DressRentalApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
