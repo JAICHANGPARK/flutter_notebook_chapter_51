97d4cf73f332e45a46dd1c1c019dabb99e7ee985
JAICHANGPARK <aristojeff@gmail.com>
2026-04-03 17:48:46 +0900

    Update dress_rental_search_page.dart

---
+++ lib/ep2555_dress_rental_app/ui/dress_rental_search_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+class DressRentalSearchPage extends StatefulWidget {
+  const DressRentalSearchPage({super.key});
+
+  @override
+  State<DressRentalSearchPage> createState() => _DressRentalSearchPageState();
+}
+
+class _DressRentalSearchPageState extends State<DressRentalSearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+
+      ],
+    );
+  }
+}
