efbe2a2d66fa0ccafda2db7868c72ffc178fb611
JAICHANGPARK <aristojeff@gmail.com>
2026-04-04 10:11:02 +0900

    Update dress_rental_search_page.dart

--- lib/ep2555_dress_rental_app/ui/dress_rental_search_page.dart
+++ lib/ep2555_dress_rental_app/ui/dress_rental_search_page.dart
@@ -11,42 +11,45 @@ class _DressRentalSearchPageState extends State<DressRentalSearchPage> {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Column(
-        children: [
-          GridView.builder(
-            itemCount: 6,
-            physics: NeverScrollableScrollPhysics(),
-            shrinkWrap: true,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              childAspectRatio: 2,
-            ),
-            itemBuilder: (context, index) {
-              return Placeholder();
-            },
-          ),
-          Row(
-            mainAxisAlignment: .spaceBetween,
-            children: [
-              Text("Featured products"),
-              Container(
-                child: Row(children: [Icon(Icons.apps), Text("View All")]),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            GridView.builder(
+              itemCount: 6,
+              physics: NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 2,
               ),
-            ],
-          ),
-          GridView.builder(
-            itemCount: 6,
-            physics: NeverScrollableScrollPhysics(),
-            shrinkWrap: true,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-            
+              itemBuilder: (context, index) {
+                return Placeholder();
+              },
             ),
-            itemBuilder: (context, index) {
-              return Placeholder();
-            },
-          ),
-        ],
+            Row(
+              mainAxisAlignment: .spaceBetween,
+              children: [
+                Text("Featured products"),
+                Container(
+                  child: Row(children: [Icon(Icons.apps), Text("View All")]),
+                ),
+              ],
+            ),
+            GridView.builder(
+              itemCount: 6,
+              physics: NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+
+              ),
+              itemBuilder: (context, index) {
+                return Placeholder();
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
