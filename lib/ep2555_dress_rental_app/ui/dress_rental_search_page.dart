833d3c2f8fe1ea048b9b2bdd1704d219d4abb6e9
JAICHANGPARK <aristojeff@gmail.com>
2026-04-04 10:14:18 +0900

    Update dress_rental_search_page.dart

--- lib/ep2555_dress_rental_app/ui/dress_rental_search_page.dart
+++ lib/ep2555_dress_rental_app/ui/dress_rental_search_page.dart
@@ -12,8 +12,9 @@ class _DressRentalSearchPageState extends State<DressRentalSearchPage> {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.symmetric(horizontal: 16),
         child: Column(
+          spacing: 16,
           children: [
             GridView.builder(
               itemCount: 6,
@@ -27,22 +28,29 @@ class _DressRentalSearchPageState extends State<DressRentalSearchPage> {
               ),
               itemBuilder: (context, index) {
                 return Container(
-
                   decoration: BoxDecoration(
                     borderRadius: .circular(8),
                     color: Colors.blue,
                   ),
                   padding: EdgeInsets.all(16),
                   child: Align(
-                      alignment: .centerLeft,
-                      child: Text("Birthday")),
+                    alignment: .centerLeft,
+                    child: Text(
+                      "Birthday",
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  ),
                 );
               },
             ),
             Row(
               mainAxisAlignment: .spaceBetween,
               children: [
-                Text("Featured products"),
+                Text(
+                  "Featured products",
+                  style: TextStyle(fontSize: 20, fontWeight: .bold,
+                  ),
+                ),
                 Container(
                   child: Row(children: [Icon(Icons.apps), Text("View All")]),
                 ),
@@ -54,7 +62,6 @@ class _DressRentalSearchPageState extends State<DressRentalSearchPage> {
               shrinkWrap: true,
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2,
-
               ),
               itemBuilder: (context, index) {
                 return Placeholder();
