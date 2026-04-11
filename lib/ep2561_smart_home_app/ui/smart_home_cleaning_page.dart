c087436890e591819f34f6f83822919697ced2b3
JAICHANGPARK <aristojeff@gmail.com>
2026-04-11 22:58:30 +0900

    Update smart_home_cleaning_page.dart

--- lib/ep2561_smart_home_app/ui/smart_home_cleaning_page.dart
+++ lib/ep2561_smart_home_app/ui/smart_home_cleaning_page.dart
@@ -14,6 +14,7 @@ class _SmartHomeCleaningPageState extends State<SmartHomeCleaningPage> {
     return Scaffold(
       body: SafeArea(
         child: Column(
+          spacing: 16,
           crossAxisAlignment: .start,
           children: [
             Row(
@@ -22,7 +23,8 @@ class _SmartHomeCleaningPageState extends State<SmartHomeCleaningPage> {
                 Expanded(child: Center(child: Text("Cleaning"),)),
                 CircleAvatar(),
               ],
-            )
+            ),
+            Expanded(child: Placeholder(),),
           ],
         ),
       ),
