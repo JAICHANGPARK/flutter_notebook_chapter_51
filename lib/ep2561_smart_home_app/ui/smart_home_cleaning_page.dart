636dbeab22f75bc23c11881191181d4d4c8ca117
JAICHANGPARK <aristojeff@gmail.com>
2026-04-11 22:53:14 +0900

    Update smart_home_cleaning_page.dart

---
+++ lib/ep2561_smart_home_app/ui/smart_home_cleaning_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+class SmartHomeCleaningPage extends StatefulWidget {
+  const SmartHomeCleaningPage({super.key});
+
+  @override
+  State<SmartHomeCleaningPage> createState() => _SmartHomeCleaningPageState();
+}
+
+class _SmartHomeCleaningPageState extends State<SmartHomeCleaningPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        
+      ),
+    );
+  }
+}
