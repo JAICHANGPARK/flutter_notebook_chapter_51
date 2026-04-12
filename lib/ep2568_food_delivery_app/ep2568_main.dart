1a32f2e370aebcb1e99bbf3dac46cdc0520cc107
JAICHANGPARK <aristojeff@gmail.com>
2026-04-12 13:22:41 +0900

    Update ep2568_main.dart

--- lib/ep2568_food_delivery_app/ep2568_main.dart
+++ lib/ep2568_food_delivery_app/ep2568_main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_51/ep2568_food_delivery_app/food_delivery_app.dart';
 
 void main() {
-
+  runApp(FoodDeliveryApp());
 }
