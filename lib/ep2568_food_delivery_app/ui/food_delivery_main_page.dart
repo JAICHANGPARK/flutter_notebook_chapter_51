1fa07023007950df989c6d796bc2c68d1ccb7654
JAICHANGPARK <aristojeff@gmail.com>
2026-04-12 13:26:37 +0900

    Update food_delivery_main_page.dart

--- lib/ep2568_food_delivery_app/ui/food_delivery_main_page.dart
+++ lib/ep2568_food_delivery_app/ui/food_delivery_main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class FoodDeliveryMainPage extends StatefulWidget {
   const FoodDeliveryMainPage({super.key});
 
@@ -12,9 +11,17 @@ class _FoodDeliveryMainPageState extends State<FoodDeliveryMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Column(children: [
-        
-      ],)),
+      body: SafeArea(
+        child: Column(
+          spacing: 16,
+          children: [
+            Container(height: 54, decoration: BoxDecoration(border: .all())),
+            Container(height: 60, decoration: BoxDecoration(border: .all())),
+            Expanded(child: Placeholder()),
+            Container(height: 80, decoration: BoxDecoration(border: .all())),
+          ],
+        ),
+      ),
     );
   }
 }
