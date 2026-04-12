9bedd77074046fcb5be89cf6b9c6fa374aad7a84
JAICHANGPARK <aristojeff@gmail.com>
2026-04-12 13:27:00 +0900

    Update food_delivery_main_page.dart

--- lib/ep2568_food_delivery_app/ui/food_delivery_main_page.dart
+++ lib/ep2568_food_delivery_app/ui/food_delivery_main_page.dart
@@ -15,8 +15,12 @@ class _FoodDeliveryMainPageState extends State<FoodDeliveryMainPage> {
         child: Column(
           spacing: 16,
           children: [
-            Container(height: 54, decoration: BoxDecoration(border: .all())),
-            Container(height: 60, decoration: BoxDecoration(border: .all())),
+            Container(
+                margin: EdgeInsets.symmetric(horizontal: 16),
+                height: 60, decoration: BoxDecoration(border: .all())),
+            Container(
+                margin: .only(left: 16),
+                height: 60, decoration: BoxDecoration(border: .all())),
             Expanded(child: Placeholder()),
             Container(height: 80, decoration: BoxDecoration(border: .all())),
           ],
