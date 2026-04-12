d03c06e9527c68821ad89e4e886d7c6d3fc101a2
JAICHANGPARK <aristojeff@gmail.com>
2026-04-12 13:22:24 +0900

    Create food_delivery_main_page.dart

---
+++ lib/ep2568_food_delivery_app/ui/food_delivery_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class FoodDeliveryMainPage extends StatefulWidget {
+  const FoodDeliveryMainPage({super.key});
+
+  @override
+  State<FoodDeliveryMainPage> createState() => _FoodDeliveryMainPageState();
+}
+
+class _FoodDeliveryMainPageState extends State<FoodDeliveryMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
