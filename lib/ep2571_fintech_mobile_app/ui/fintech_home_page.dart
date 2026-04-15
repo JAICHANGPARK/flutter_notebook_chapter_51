7386a7e8f9884771f58f76ad2146f469efaac0b9
JAICHANGPARK <aristojeff@gmail.com>
2026-04-15 18:32:50 +0900

    Update fintech_home_page.dart

--- lib/ep2571_fintech_mobile_app/ui/fintech_home_page.dart
+++ lib/ep2571_fintech_mobile_app/ui/fintech_home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class FintechHomePage extends StatefulWidget {
   const FintechHomePage({super.key});
 
@@ -12,17 +11,18 @@ class _FintechHomePageState extends State<FintechHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       body: Column(
         children: [
           Row(
             children: [
               CircleAvatar(),
               Expanded(child: Text("Hello, Dream")),
-              IconButton(onPressed: (){}, icon: Icon(Icons.notifications_none)
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.notifications_none),
+              ),
             ],
-          )
-
+          ),
         ],
       ),
     );
