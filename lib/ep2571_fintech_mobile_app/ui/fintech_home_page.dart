9144e70265fbd5354d3f6c736fb71bbe5430d3b2
JAICHANGPARK <aristojeff@gmail.com>
2026-04-15 18:43:27 +0900

    Update fintech_home_page.dart

--- lib/ep2571_fintech_mobile_app/ui/fintech_home_page.dart
+++ lib/ep2571_fintech_mobile_app/ui/fintech_home_page.dart
@@ -11,79 +11,89 @@ class _FintechHomePageState extends State<FintechHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: .start,
-        spacing: 12,
-        children: [
-          Row(
-            children: [
-              CircleAvatar(),
-              Expanded(child: Text("Hello, Dream")),
-              IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.notifications_none),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: .start,
+          spacing: 12,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Row(
+                spacing: 12,
+                children: [
+                  CircleAvatar(
+                    radius: 22,
+                  ),
+                  Expanded(child: Text("Hello, Dream",style: TextStyle(
+                    fontSize: 18,
+                  ),)),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.notifications_none),
+                  ),
+                ],
               ),
-            ],
-          ),
-          Text("Total balance"),
-          Text("\$8 600"),
-          Row(
-            children: [
-              Text("CARDS"),
-              TextButton(onPressed: () {}, child: Text("Add +")),
-            ],
-          ),
-          SizedBox(height: 180, child: Placeholder()),
-          Text("FINANCE"),
-          SizedBox(height: 120, child: Placeholder()),
-          SizedBox(height: 140, child: Placeholder()),
-          Container(
-            decoration: BoxDecoration(color: Colors.grey[100]!),
-            padding: EdgeInsets.all(16),
-            child: Column(
+            ),
+            Text("Total balance"),
+            Text("\$8 600"),
+            Row(
               children: [
-                Row(
-                  children: [
-                    Text("LAST TRANSACTIONS"),
-                    Spacer(),
-                    Text("See all"),
-                    Icon(Icons.keyboard_arrow_right),
-                  ],
-                ),
-                Column(
-                  children: List.generate(6, (idx) {
-                    return Row(children: [
-                      Container(
-                        height: 42,
-                        width: 42,
-                        child: Placeholder(),
-                      ),
-                      Expanded(child: Column(children: [
-                        Row(
-                          mainAxisAlignment: .spaceBetween,
-                          children: [
-                            Text("Supermarket"),
-                            Text("-40\$"),
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: .spaceBetween,
-                          children: [
-
-                            Text("Today, 12:00"),
-                            Text("payment")
-                          ],
-                        )
-
-                      ],))
-
-                    ]);
-                  }),
-                ),
+                Text("CARDS"),
+                TextButton(onPressed: () {}, child: Text("Add +")),
               ],
             ),
-          ),
-        ],
+            SizedBox(height: 180, child: Placeholder()),
+            Text("FINANCE"),
+            SizedBox(height: 120, child: Placeholder()),
+            SizedBox(height: 140, child: Placeholder()),
+            Container(
+              decoration: BoxDecoration(color: Colors.grey[100]!),
+              padding: EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text("LAST TRANSACTIONS"),
+                      Spacer(),
+                      Text("See all"),
+                      Icon(Icons.keyboard_arrow_right),
+                    ],
+                  ),
+                  Column(
+                    children: List.generate(6, (idx) {
+                      return Row(children: [
+                        Container(
+                          height: 42,
+                          width: 42,
+                          child: Placeholder(),
+                        ),
+                        Expanded(child: Column(children: [
+                          Row(
+                            mainAxisAlignment: .spaceBetween,
+                            children: [
+                              Text("Supermarket"),
+                              Text("-40\$"),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: .spaceBetween,
+                            children: [
+        
+                              Text("Today, 12:00"),
+                              Text("payment")
+                            ],
+                          )
+        
+                        ],))
+        
+                      ]);
+                    }),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
