5fa0e1e9072039120e5f3ec2676e35b7c3dfe0ed
JAICHANGPARK <aristojeff@gmail.com>
2026-04-15 18:27:58 +0900

    Update ep2571_main.dart

--- lib/ep2571_fintech_mobile_app/ep2571_main.dart
+++ lib/ep2571_fintech_mobile_app/ep2571_main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_51/ep2571_fintech_mobile_app/fintech_app.dart';
 
 
 void main() {
-  runApp()
+  runApp(FintechApp());
 }
