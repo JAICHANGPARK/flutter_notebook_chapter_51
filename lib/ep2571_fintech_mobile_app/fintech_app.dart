88a850099beb4dfd87f6c95202df4ed87c430a76
JAICHANGPARK <aristojeff@gmail.com>
2026-04-15 18:27:44 +0900

    Update fintech_app.dart

---
+++ lib/ep2571_fintech_mobile_app/fintech_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class FintechApp extends StatelessWidget {
+  const FintechApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
