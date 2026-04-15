2c9dac96e15e03d9517d0459b87ae4b478830bfc
JAICHANGPARK <aristojeff@gmail.com>
2026-04-15 18:28:18 +0900

    Create fintech_home_page.dart

---
+++ lib/ep2571_fintech_mobile_app/ui/fintech_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class FintechHomePage extends StatefulWidget {
+  const FintechHomePage({super.key});
+
+  @override
+  State<FintechHomePage> createState() => _FintechHomePageState();
+}
+
+class _FintechHomePageState extends State<FintechHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
