6964da7242ae06168556f49e0ace046c20205d44
JAICHANGPARK <aristojeff@gmail.com>
2026-04-15 18:29:34 +0900

    Update fintech_app.dart

--- lib/ep2571_fintech_mobile_app/fintech_app.dart
+++ lib/ep2571_fintech_mobile_app/fintech_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_51/ep2571_fintech_mobile_app/ui/fintech_home_page.dart';
 
 
 class FintechApp extends StatelessWidget {
@@ -7,7 +8,7 @@ class FintechApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+        home: FintechHomePage(),
     );
   }
 }
