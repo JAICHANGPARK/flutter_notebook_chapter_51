5c0553ffb9c36b04bd2ee8324b64a87dafe0a9f1
JAICHANGPARK <aristojeff@gmail.com>
2026-04-18 11:19:25 +0900

    Update social_discovery_home_page.dart

--- lib/ep2574_social_discovery_app/ui/social_discovery_home_page.dart
+++ lib/ep2574_social_discovery_app/ui/social_discovery_home_page.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-
 class SocialDiscoveryHomePage extends StatefulWidget {
   const SocialDiscoveryHomePage({super.key});
 
   @override
-  State<SocialDiscoveryHomePage> createState() => _SocialDiscoveryHomePageState();
+  State<SocialDiscoveryHomePage> createState() =>
+      _SocialDiscoveryHomePageState();
 }
 
 class _SocialDiscoveryHomePageState extends State<SocialDiscoveryHomePage> {
@@ -14,13 +14,19 @@ class _SocialDiscoveryHomePageState extends State<SocialDiscoveryHomePage> {
     return Scaffold(
       body: Stack(
         children: [
-          Positioned.fill(child: Container(decoration: BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Color.fromRGBO(199, 208, 207, 1),
-              Color.fromRGBO(232, 237, 235, 1),
-              Color.fromRGBO(112, 142, 128, 1),
-            ],)
-          ),))
+          Positioned.fill(
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromRGBO(199, 208, 207, 1),
+                    Color.fromRGBO(232, 237, 235, 1),
+                    Color.fromRGBO(112, 142, 128, 1),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
