8a221cb0b784ae42fba8b4c1e801722041fcc85d
JAICHANGPARK <aristojeff@gmail.com>
2026-04-18 11:12:22 +0900

    Update social_discovery_home_page.dart

---
+++ lib/ep2574_social_discovery_app/ui/social_discovery_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class SocialDiscoveryHomePage extends StatefulWidget {
+  const SocialDiscoveryHomePage({super.key});
+
+  @override
+  State<SocialDiscoveryHomePage> createState() => _SocialDiscoveryHomePageState();
+}
+
+class _SocialDiscoveryHomePageState extends State<SocialDiscoveryHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
