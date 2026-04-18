628d0366876032cea859e3df39c223c3ab35e7c5
JAICHANGPARK <aristojeff@gmail.com>
2026-04-18 11:13:12 +0900

    Update ep2574_main.dart

--- lib/ep2574_social_discovery_app/ep2574_main.dart
+++ lib/ep2574_social_discovery_app/ep2574_main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_51/ep2574_social_discovery_app/social_discovery_app.dart';
 
 
 void main() {
-  runApp();
+  runApp(SocialDiscoveryApp());
 }
