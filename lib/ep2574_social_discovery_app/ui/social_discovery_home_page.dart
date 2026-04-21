e41913eead84b9792ec891c435ca9c11c413c4f0
JAICHANGPARK <aristojeff@gmail.com>
2026-04-21 18:31:42 +0900

    Update social_discovery_home_page.dart

--- lib/ep2574_social_discovery_app/ui/social_discovery_home_page.dart
+++ lib/ep2574_social_discovery_app/ui/social_discovery_home_page.dart
@@ -322,7 +322,38 @@ class _SocialDiscoveryHomePageState extends State<SocialDiscoveryHomePage> {
                                       ],
                                     ),
                                   ),
-                                  Positioned(child: Container())
+                                  Positioned(child: Container(
+                                    decoration: BoxDecoration(
+                                      color: Color.fromRGBO(199, 209, 203, 1),
+                                    ),
+                                    padding: EdgeInsets.all(4),
+                                    child:   Container(
+                                      padding: EdgeInsets.all(10),
+                                      decoration: BoxDecoration(
+                                        border: .all(
+                                          color: Colors.white38,
+                                          width: .5,
+                                        ),
+                                        color: Color.fromRGBO(
+                                          199,
+                                          209,
+                                          203,
+                                          1,
+                                        ),
+                                        boxShadow: [
+                                          BoxShadow(
+                                            color: Colors.black12,
+                                            blurRadius: 1,
+                                            spreadRadius: 2,
+                                          ),
+                                        ],
+                                      ),
+                                      child: Icon(
+                                        Icons.favorite,
+                                        color: Color.fromRGBO(71, 111, 91, 1),
+                                      ),
+                                    ),
+                                  ),)
                                 ],
                               ),
                             );
