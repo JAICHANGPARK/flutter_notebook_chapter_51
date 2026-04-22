d1970828dfaaed501ddccff4c8e0be9368213400
JAICHANGPARK <aristojeff@gmail.com>
2026-04-22 22:25:22 +0900

    Update bike_rental_home_page.dart

--- lib/ep2578_bike_rental_app/ui/bike_rental_home_page.dart
+++ lib/ep2578_bike_rental_app/ui/bike_rental_home_page.dart
@@ -58,123 +58,123 @@ class _BikeRentalHomePageState extends State<BikeRentalHomePage> {
                       ],
                     ),
                   ),
-                  Gap(24),
+                  Gap(18),
                   Expanded(child: SingleChildScrollView(
                     padding: .zero,
                       child: Column(
                     crossAxisAlignment: .start,
                     children: [
+                      Container(
+                        padding: .only(left: 16),
+                        height: 64,
+                        child: ListView(
+                          scrollDirection: .horizontal,
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                shape: .circle,
+                                border: .all(color: Colors.grey[200]!),
+                              ),
+                              padding: .all(14),
+                              child: Icon(Icons.search),
+                              margin: .only(right: 12),
+                            ),
+                            Container(
+                              decoration: ShapeDecoration(
+                                shape: StadiumBorder(),
+                                color: Colors.black,
+                              ),
+                              padding: EdgeInsets.symmetric(horizontal: 18),
+                              margin: .only(right: 12),
+                              child: Row(
+                                spacing: 5,
+                                children: [
+                                  Icon(Icons.pedal_bike, color: Colors.white),
+                                  Text(
+                                    "E-Bicycles",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Container(
+                              decoration: ShapeDecoration(
+                                shape: StadiumBorder(),
+                                color: Colors.grey[200]!,
+                              ),
+                              padding: EdgeInsets.symmetric(horizontal: 18),
+                              margin: .only(right: 12),
+                              child: Row(
+                                spacing: 5,
+                                children: [
+                                  Icon(Icons.electric_scooter_outlined),
+                                  Text("E-Scooters"),
+                                ],
+                              ),
+                            ),
+                            Container(
+                              decoration: ShapeDecoration(
+                                shape: StadiumBorder(),
+                                color: Colors.grey[200]!,
+                              ),
+                              padding: EdgeInsets.symmetric(horizontal: 18),
+                              margin: .only(right: 12),
+                              child: Row(
+                                spacing: 5,
+                                children: [
+                                  Icon(Icons.electric_scooter_outlined),
+                                  Text("E-Scooters"),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 520,
+                        child: ListView.builder(
+                          itemCount: 10,
+                          scrollDirection: .horizontal,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              width: 240,
+                              decoration: ShapeDecoration(
+                                shape: RoundedSuperellipseBorder(
+                                  borderRadius: .circular(24),
+                                ),
+                                color: Color.fromRGBO(227, 230, 252, 1),
+                              ),
+                              child: Stack(
+                                children: [
+                                  Positioned(
+                                      left: 16,
+                                      top: 16,
+                                      bottom: 16,
+                                      right: 16,
+                                      child: Column(children: [
 
+                                      ],))
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Text("Find Bikes Nearby"),
+                          Spacer(),
+                          Text("View more"),
+                          Icon(Icons.keyboard_arrow_right),
+                        ],
+                      ),
+                      Container(height: 120, child: Placeholder()),
                     ],
                   )),),
-                  Container(
-                    padding: .only(left: 16),
-                    height: 64,
-                    child: ListView(
-                      scrollDirection: .horizontal,
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            shape: .circle,
-                            border: .all(color: Colors.grey[200]!),
-                          ),
-                          padding: .all(14),
-                          child: Icon(Icons.search),
-                          margin: .only(right: 12),
-                        ),
-                        Container(
-                          decoration: ShapeDecoration(
-                            shape: StadiumBorder(),
-                            color: Colors.black,
-                          ),
-                          padding: EdgeInsets.symmetric(horizontal: 18),
-                          margin: .only(right: 12),
-                          child: Row(
-                            spacing: 5,
-                            children: [
-                              Icon(Icons.pedal_bike, color: Colors.white),
-                              Text(
-                                "E-Bicycles",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 15,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Container(
-                          decoration: ShapeDecoration(
-                            shape: StadiumBorder(),
-                            color: Colors.grey[200]!,
-                          ),
-                          padding: EdgeInsets.symmetric(horizontal: 18),
-                          margin: .only(right: 12),
-                          child: Row(
-                            spacing: 5,
-                            children: [
-                              Icon(Icons.electric_scooter_outlined),
-                              Text("E-Scooters"),
-                            ],
-                          ),
-                        ),
-                        Container(
-                          decoration: ShapeDecoration(
-                            shape: StadiumBorder(),
-                            color: Colors.grey[200]!,
-                          ),
-                          padding: EdgeInsets.symmetric(horizontal: 18),
-                          margin: .only(right: 12),
-                          child: Row(
-                            spacing: 5,
-                            children: [
-                              Icon(Icons.electric_scooter_outlined),
-                              Text("E-Scooters"),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    height: 520,
-                    child: ListView.builder(
-                      itemCount: 10,
-                      scrollDirection: .horizontal,
-                      itemBuilder: (context, index) {
-                        return Container(
-                          width: 240,
-                          decoration: ShapeDecoration(
-                            shape: RoundedSuperellipseBorder(
-                              borderRadius: .circular(24),
-                            ),
-                            color: Color.fromRGBO(227, 230, 252, 1),
-                          ),
-                          child: Stack(
-                            children: [
-                              Positioned(
-                                  left: 16,
-                                  top: 16,
-                                  bottom: 16,
-                                  right: 16,
-                                  child: Column(children: [
 
-                              ],))
-                            ],
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                  Row(
-                    children: [
-                      Text("Find Bikes Nearby"),
-                      Spacer(),
-                      Text("View more"),
-                      Icon(Icons.keyboard_arrow_right),
-                    ],
-                  ),
-                  Container(height: 120, child: Placeholder()),
                 ],
               ),
             ),
