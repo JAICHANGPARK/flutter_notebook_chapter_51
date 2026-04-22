24ca601715cce99834360752b89f4ed8b858e7dc
JAICHANGPARK <aristojeff@gmail.com>
2026-04-22 21:46:30 +0900

    Update bike_rental_start_page.dart

---
+++ lib/ep2578_bike_rental_app/ui/bike_rental_start_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class BikeRentalStartPage extends StatefulWidget {
+  const BikeRentalStartPage({super.key});
+
+  @override
+  State<BikeRentalStartPage> createState() => _BikeRentalStartPageState();
+}
+
+class _BikeRentalStartPageState extends State<BikeRentalStartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
