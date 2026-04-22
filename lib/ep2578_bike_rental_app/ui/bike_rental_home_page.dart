1a1eff584bd86ef7fcff564344205e029c59fd0f
JAICHANGPARK <aristojeff@gmail.com>
2026-04-22 22:06:03 +0900

    Update bike_rental_home_page.dart

--- lib/ep2578_bike_rental_app/ui/bike_rental_home_page.dart
+++ lib/ep2578_bike_rental_app/ui/bike_rental_home_page.dart
@@ -11,6 +11,8 @@ class BikeRentalHomePage extends StatefulWidget {
 class _BikeRentalHomePageState extends State<BikeRentalHomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+
+    );
   }
 }
