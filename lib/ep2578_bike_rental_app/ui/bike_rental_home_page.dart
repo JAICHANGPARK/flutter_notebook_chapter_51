42bc2115c952471672ba678b5bc95bae69255c54
JAICHANGPARK <aristojeff@gmail.com>
2026-04-22 22:16:14 +0900

    Update bike_rental_home_page.dart

--- lib/ep2578_bike_rental_app/ui/bike_rental_home_page.dart
+++ lib/ep2578_bike_rental_app/ui/bike_rental_home_page.dart
@@ -16,53 +16,56 @@ class _BikeRentalHomePageState extends State<BikeRentalHomePage> {
       backgroundColor: Colors.white,
       body: Stack(
         children: [
-          SafeArea(
-            child: Column(
-              crossAxisAlignment: .start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 12),
-                  child: Row(
-                    spacing: 12,
-                    children: [
-                      CircleAvatar(radius: 30),
-                      Expanded(
-                        child: Column(
-                          spacing: 6,
-                          crossAxisAlignment: .start,
-                          children: [
-                            Text("Hello, Dream", style: TextStyle(fontSize: 16)),
-                            Text(
-                              "Good Morning!",
-                              style: TextStyle(fontSize: 24, fontWeight: .bold),
-                            ),
-                          ],
-                        ),
-                      ),
-                      CircleAvatar(
-                        radius: 30,
-                        child: Badge(
-                          child: HugeIcon(
-                            icon: HugeIcons.strokeRoundedNotification01,
+          Positioned.fill(
+            top: 12,
+            child: SafeArea(
+              child: Column(
+                crossAxisAlignment: .start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 12),
+                    child: Row(
+                      spacing: 12,
+                      children: [
+                        CircleAvatar(radius: 30),
+                        Expanded(
+                          child: Column(
+                            spacing: 6,
+                            crossAxisAlignment: .start,
+                            children: [
+                              Text("Hello, Dream", style: TextStyle(fontSize: 16)),
+                              Text(
+                                "Good Morning!",
+                                style: TextStyle(fontSize: 24, fontWeight: .bold),
+                              ),
+                            ],
                           ),
                         ),
-                      ),
+                        CircleAvatar(
+                          radius: 30,
+                          child: Badge(
+                            child: HugeIcon(
+                              icon: HugeIcons.strokeRoundedNotification01,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Gap(24),
+                  SizedBox(height: 72, child: Placeholder()),
+                  Container(height: 520, child: Placeholder()),
+                  Row(
+                    children: [
+                      Text("Find Bikes Nearby"),
+                      Spacer(),
+                      Text("View more"),
+                      Icon(Icons.keyboard_arrow_right),
                     ],
                   ),
-                ),
-                Gap(24),
-                SizedBox(height: 72, child: Placeholder()),
-                Container(height: 520, child: Placeholder()),
-                Row(
-                  children: [
-                    Text("Find Bikes Nearby"),
-                    Spacer(),
-                    Text("View more"),
-                    Icon(Icons.keyboard_arrow_right),
-                  ],
-                ),
-                Container(height: 120, child: Placeholder()),
-              ],
+                  Container(height: 120, child: Placeholder()),
+                ],
+              ),
             ),
           ),
         ],
