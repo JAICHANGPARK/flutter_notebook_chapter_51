bc451c53b4a0d4b1c6ea67b5a9237a2c1f128cd9
JAICHANGPARK <aristojeff@gmail.com>
2026-04-22 21:45:53 +0900

    Update ep2578_main.dart

--- lib/ep2578_bike_rental_app/ep2578_main.dart
+++ lib/ep2578_bike_rental_app/ep2578_main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_51/ep2578_bike_rental_app/bike_rental_app.dart';
 
 void main() {
-  
+  runApp(BikeRentalApp());
 }
