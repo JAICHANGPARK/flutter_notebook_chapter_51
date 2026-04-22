e962a310fb427462cf8ca159c1ae28c0da6ff743
JAICHANGPARK <aristojeff@gmail.com>
2026-04-22 22:11:11 +0900

    Update bike_rental_home_page.dart

--- lib/ep2578_bike_rental_app/ui/bike_rental_home_page.dart
+++ lib/ep2578_bike_rental_app/ui/bike_rental_home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class BikeRentalHomePage extends StatefulWidget {
   const BikeRentalHomePage({super.key});
@@ -12,33 +13,38 @@ class _BikeRentalHomePageState extends State<BikeRentalHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Column(
-          children: [
-            Row(
+      body: Stack(
+        children: [
+          SafeArea(
+            child: Column(
               children: [
-                CircleAvatar(
-                  radius: 32,
+                Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 32,
+                    ),
+                    Expanded(child: Column(children: [])),
+                    CircleAvatar(
+                      radius: 32,
+                    ),
+                  ],
                 ),
-                Expanded(child: Column(children: [])),
-                CircleAvatar(
-                  radius: 32,
+                Gap(24),
+                SizedBox(height: 72, child: Placeholder()),
+                Container(height: 520, child: Placeholder()),
+                Row(
+                  children: [
+                    Text("Find Bikes Nearby"),
+                    Spacer(),
+                    Text("View more"),
+                    Icon(Icons.keyboard_arrow_right),
+                  ],
                 ),
+                Container(height: 120, child: Placeholder()),
               ],
             ),
-            SizedBox(height: 72, child: Placeholder()),
-            Container(height: 520, child: Placeholder()),
-            Row(
-              children: [
-                Text("Find Bikes Nearby"),
-                Spacer(),
-                Text("View more"),
-                Icon(Icons.keyboard_arrow_right),
-              ],
-            ),
-            Container(height: 120, child: Placeholder()),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
