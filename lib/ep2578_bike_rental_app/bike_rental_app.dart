2395c724698d7c0ce780323864adb1fcef0aeeda
JAICHANGPARK <aristojeff@gmail.com>
2026-04-22 21:51:05 +0900

    Update bike_rental_app.dart

--- lib/ep2578_bike_rental_app/bike_rental_app.dart
+++ lib/ep2578_bike_rental_app/bike_rental_app.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'ui/bike_rental_start_page.dart';
+
 class BikeRentalApp extends StatelessWidget {
   const BikeRentalApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Column(children: []));
+    return MaterialApp(home: BikeRentalStartPage());
   }
 }
