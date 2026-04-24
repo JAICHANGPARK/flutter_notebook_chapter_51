f8cc2754c31866764b1e18a6afd5dc54ecbdfc53
Dreamwalker <aristojeff@gmail.com>
2026-04-24 23:22:06 +0900

    ep2574_main.dart 업데이트

--- lib/ep2574_social_discovery_app/ep2574_main.dart
+++ lib/ep2574_social_discovery_app/ep2574_main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_51/ep2574_social_discovery_app/social_discovery_app.dart';
 
-
 void main() {
   runApp(SocialDiscoveryApp());
 }
