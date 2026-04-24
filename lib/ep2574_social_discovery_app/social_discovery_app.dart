641d80207db4dafcae6826355000d740753b779c
Dreamwalker <aristojeff@gmail.com>
2026-04-24 23:21:53 +0900

    social_discovery_app.dart 업데이트

--- lib/ep2574_social_discovery_app/social_discovery_app.dart
+++ lib/ep2574_social_discovery_app/social_discovery_app.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_51/ep2574_social_discovery_app/ui/social_discovery_home_page.dart';
 
-
 class SocialDiscoveryApp extends StatelessWidget {
   const SocialDiscoveryApp({super.key});
 
