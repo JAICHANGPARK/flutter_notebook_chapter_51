2452642cac9f64b804c8250904c13a75fc1aaf10
JAICHANGPARK <aristojeff@gmail.com>
2026-05-04 18:25:03 +0900

    Update water_bottle_app.dart

---
+++ lib/ep2588_water_bottle_app/water_bottle_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class WaterBottleApp extends StatelessWidget {
+  const WaterBottleApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
