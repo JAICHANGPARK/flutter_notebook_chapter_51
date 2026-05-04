8ebe22da1940fe2b0231ed76d3c6ea2ff09bc0d0
JAICHANGPARK <aristojeff@gmail.com>
2026-05-04 18:26:37 +0900

    Update ep2588_main.dart

--- lib/ep2588_water_bottle_app/ep2588_main.dart
+++ lib/ep2588_water_bottle_app/ep2588_main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_51/ep2588_water_bottle_app/water_bottle_app.dart';
 
 
 void main() {
-  
+  runApp(WaterBottleApp());
 }
