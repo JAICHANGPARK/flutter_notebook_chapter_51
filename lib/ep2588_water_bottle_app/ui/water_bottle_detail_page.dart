d29539e6cba25a9802c8c80c3f12060043a46ddd
JAICHANGPARK <aristojeff@gmail.com>
2026-05-07 18:09:14 +0900

    Create water_bottle_detail_page.dart

---
+++ lib/ep2588_water_bottle_app/ui/water_bottle_detail_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class WaterBottleDetailPage extends StatefulWidget {
+  const WaterBottleDetailPage({super.key});
+
+  @override
+  State<WaterBottleDetailPage> createState() => _WaterBottleDetailPageState();
+}
+
+class _WaterBottleDetailPageState extends State<WaterBottleDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
