2e9c280233aa8e25c95f0de1d5e0511baa3ec5e6
JAICHANGPARK <aristojeff@gmail.com>
2026-05-11 23:03:42 +0900

    Update food_delivery_home_page.dart

--- lib/ep2595_food_delivery_app/ui/food_delivery_home_page.dart
+++ lib/ep2595_food_delivery_app/ui/food_delivery_home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class FoodDeliveryHomePage extends StatefulWidget {
   const FoodDeliveryHomePage({super.key});
 
@@ -12,7 +11,13 @@ class _FoodDeliveryHomePageState extends State<FoodDeliveryHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: SafeArea(child: Column(children: [])),
+          ),
+        ],
+      ),
     );
   }
 }
