28cab53422696bd9a1f36bc4c4ea759d653453a9
JAICHANGPARK <aristojeff@gmail.com>
2026-05-11 23:02:09 +0900

    Update healthy_food_delivery_app.dart

--- lib/ep2595_food_delivery_app/healthy_food_delivery_app.dart
+++ lib/ep2595_food_delivery_app/healthy_food_delivery_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_51/ep2595_food_delivery_app/ui/food_delivery_home_page.dart';
 
 
 class HealthyFoodDeliveryApp extends StatelessWidget {
@@ -7,7 +8,7 @@ class HealthyFoodDeliveryApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      home: FoodDeliveryHomePage(),
     );
   }
 }
