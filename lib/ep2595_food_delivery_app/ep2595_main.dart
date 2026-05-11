9791473d504c15653998c3b4c9524edf3a08c1f9
JAICHANGPARK <aristojeff@gmail.com>
2026-05-11 23:00:13 +0900

    Update ep2595_main.dart

--- lib/ep2595_food_delivery_app/ep2595_main.dart
+++ lib/ep2595_food_delivery_app/ep2595_main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_51/ep2595_food_delivery_app/healthy_food_delivery_app.dart';
 
 
 void main() {
-  
+    runApp(HealthyFoodDeliveryApp());
 }
