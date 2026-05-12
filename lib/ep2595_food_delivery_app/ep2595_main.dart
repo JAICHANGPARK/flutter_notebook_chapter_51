3b1598b92e14d9491341a37a1a5b9cb6c98091d8
Dreamwalker <aristojeff@gmail.com>
2026-05-12 21:16:59 +0900

    ep2595_main.dart 업데이트

--- lib/ep2595_food_delivery_app/ep2595_main.dart
+++ lib/ep2595_food_delivery_app/ep2595_main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_51/ep2595_food_delivery_app/healthy_food_delivery_app.dart';
 
-
 void main() {
     runApp(HealthyFoodDeliveryApp());
 }
