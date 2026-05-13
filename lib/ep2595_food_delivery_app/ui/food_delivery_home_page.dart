8859716c904b3171d69f9ce9a7f2d45c97971c75
JAICHANGPARK <aristojeff@gmail.com>
2026-05-13 18:31:43 +0900

    Update food_delivery_home_page.dart

--- lib/ep2595_food_delivery_app/ui/food_delivery_home_page.dart
+++ lib/ep2595_food_delivery_app/ui/food_delivery_home_page.dart
@@ -19,27 +19,30 @@ class _FoodDeliveryHomePageState extends State<FoodDeliveryHomePage> {
             child: SafeArea(
               child: Column(
                 children: [
-                  Row(
-                    children: [
-                      CircleAvatar(backgroundColor: Colors.white, radius: 24),
-                      Expanded(
-                        child: Column(
-                          children: [
-                            Text("Location"),
-                            Row(
-                              mainAxisAlignment: .center,
-                              crossAxisAlignment: .center,
-                              children: [
-                                Icon(Icons.location_on_outlined),
-                                Text("New York, USA"),
-                                Icon(Icons.keyboard_arrow_down),
-                              ],
-                            ),
-                          ],
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      children: [
+                        CircleAvatar(backgroundColor: Colors.white, radius: 24),
+                        Expanded(
+                          child: Column(
+                            children: [
+                              Text("Location"),
+                              Row(
+                                mainAxisAlignment: .center,
+                                crossAxisAlignment: .center,
+                                children: [
+                                  Icon(Icons.location_on_outlined),
+                                  Text("New York, USA"),
+                                  Icon(Icons.keyboard_arrow_down),
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                      CircleAvatar(backgroundColor: Colors.white, radius: 24),
-                    ],
+                        CircleAvatar(backgroundColor: Colors.white, radius: 24),
+                      ],
+                    ),
                   ),
                   SizedBox(height: 42, child: Placeholder()),
                   Expanded(child: SingleChildScrollView(
