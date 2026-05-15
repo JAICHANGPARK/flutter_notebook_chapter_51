bdd08da8fad06543d085cd0096e0f14909daa2f7
JAICHANGPARK <aristojeff@gmail.com>
2026-05-15 12:34:56 +0900

    Update food_delivery_home_page.dart

--- lib/ep2595_food_delivery_app/ui/food_delivery_home_page.dart
+++ lib/ep2595_food_delivery_app/ui/food_delivery_home_page.dart
@@ -329,7 +329,12 @@ class _FoodDeliveryHomePageState extends State<FoodDeliveryHomePage> {
                 color: Color.fromRGBO(252, 181, 30, 1),
               ),
               child: Row(
-                children: [HugeIcon(icon: HugeIcons.strokeRoundedHome01)],
+                children: [HugeIcon(icon: HugeIcons.strokeRoundedHome01),
+                  HugeIcon(icon: HugeIcons.strokeRoundedHandBag01),
+                  HugeIcon(icon: HugeIcons.strokeRoundedSearch01),
+                  HugeIcon(icon: HugeIcons.strokeRoundedChatUser01),
+                  HugeIcon(icon: HugeIcons.strokeRoundedHome01)
+                ],
               ),
             ),
           ),
