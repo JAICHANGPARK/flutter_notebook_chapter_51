6c5060c9b9bfbc9a329b4c25ca783bf29404b1d0
JAICHANGPARK <aristojeff@gmail.com>
2026-05-15 13:32:50 +0900

    Update food_delivery_home_page.dart

--- lib/ep2595_food_delivery_app/ui/food_delivery_home_page.dart
+++ lib/ep2595_food_delivery_app/ui/food_delivery_home_page.dart
@@ -382,21 +382,25 @@ class _FoodDeliveryHomePageState extends State<FoodDeliveryHomePage> {
                     child: Container(
                       decoration: ShapeDecoration(
                         shape: StadiumBorder(),
-                        color: Colors.black,
+                        color: pageNum == 1 ? Colors.black : Colors.transparent,
                       ),
                       padding: .symmetric(horizontal: 16, vertical: 12),
                       child: Row(
                         spacing: 12,
-                        children: [
+                        children: pageNum == 1 ? [
                           HugeIcon(
                             icon: HugeIcons.strokeRoundedHandBag01,
                             size: 28,
+                            color: Colors.white,
                           ),
                           Text(
                             "Cart",
                             style: TextStyle(fontSize: 16, color: Colors.white),
                           ),
-                        ],
+                        ] : [HugeIcon(
+                          icon: HugeIcons.strokeRoundedHandBag01,
+                          size: 28,
+                        ),],
                       ),
                     ),
                   ),
